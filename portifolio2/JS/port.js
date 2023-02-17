57d302962cfb5da296562924cd0eb9c2b9504733
alefalves4867 <123469430+alefalves4867@users.noreply.github.com>
2023-02-17 12:21:41 -0300

    update

--- portifolio2/JS/port.js
+++ portifolio2/JS/port.js
@@ -5,9 +5,13 @@ menuControl.addEventListener('click', () => {
     menuControl.classList.toggle('menu1On')//Adiciona a class onC no elemento MenuC
     header.classList.toggle('tamanhoFull')//Adiciona o tamanho maximo de header
     if (header.classList.contains('tamanhoFull')) {
-    setTimeout(() => {menuNav.classList.toggle('hidden')}, 350)
+    setTimeout(() => {menuNav.classList.toggle('hidden')}, 320)
     } else {
         menuNav.classList.toggle('hidden')
     }
     /*Se header tiver a class tamanhoC ele atrása a execução de menuNav.toggle se não ele já adiciona imediatamente a class hidden*/
 })
+
+function load() {
+    console.log('teste de carga')
+}
