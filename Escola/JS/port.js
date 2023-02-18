e400a549037f651cf6a0ab30f896c230a2213e70
Alef Alves de Araujo <123469430+alefalves4867@users.noreply.github.com>
2023-02-18 13:55:08 -0300

    CETI

---
+++ Escola/JS/port.js
@@ -0,0 +1,17 @@
+const menuControl = document.getElementById('MenuControl')
+const menuNav = document.getElementById('menuNav')
+const header = document.getElementById('header')
+menuControl.addEventListener('click', () => {
+    menuControl.classList.toggle('menu1On')//Adiciona a class onC no elemento MenuC
+    header.classList.toggle('tamanhoFull')//Adiciona o tamanho maximo de header
+    if (header.classList.contains('tamanhoFull')) {
+    setTimeout(() => {menuNav.classList.toggle('hidden')}, 320)
+    } else {
+        menuNav.classList.toggle('hidden')
+    }
+    /*Se header tiver a class tamanhoC ele atrása a execução de menuNav.toggle se não ele já adiciona imediatamente a class hidden*/
+})
+
+function load() {
+    console.log('teste de carga')
+}
