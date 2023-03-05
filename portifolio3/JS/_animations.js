50cbcda2b5f4ac09548ee91b9c145b72e7ce9f1b
alefalves4867 <alefalves4867@gmail.com>
2023-03-05 12:56:05 -0300

    atualizei

--- portifolio3/JS/_animations.js
+++ portifolio3/JS/_animations.js
@@ -1,6 +1,9 @@
 const alfbt = document.querySelector('#alfbt')
-
+const phi = document.querySelector('.phi')
 
 function load() {
   alfbt.classList.add('alfbt-animation1')
+  setTimeout(() => {
+    phi.classList.add('phi-normal')
+  }, 1550)
 }
