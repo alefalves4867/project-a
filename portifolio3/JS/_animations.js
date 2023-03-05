77d95f45e8d5d8c3ddedad1d86b86cf488f90981
alefalves4867 <alefalves4867@gmail.com>
2023-03-05 14:14:09 -0300

    atualizei a animação

--- portifolio3/JS/_animations.js
+++ portifolio3/JS/_animations.js
@@ -1,9 +1,22 @@
 const alfbt = document.querySelector('#alfbt')
+const alpha = document.querySelector('.alpha')
+const lambda = document.querySelector('.lambda')
+const epsilon = document.querySelector('.epsilon')
 const phi = document.querySelector('.phi')
 
+
+
+
 function load() {
-  alfbt.classList.add('alfbt-animation1')
+  alpha.classList.add('alfbt-animation1')
   setTimeout(() => {
-    phi.classList.add('phi-normal')
-  }, 1550)
+    phi.classList.add('opacity-normal')
+    setTimeout(() => {
+      epsilon.classList.add('opacity-normal')
+      setTimeout(() => {
+        lambda.classList.add('opacity-normal')
+        
+      }, 200)
+    }, 250)
+  }, 500)
 }
